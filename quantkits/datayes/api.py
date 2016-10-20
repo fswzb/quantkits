d350aebe8e68a2936bb87853443996517931408d
josephchen <joseph.chen@digitcube.com>
2016-10-20 16:43:18 +0800

    quantkits init

---
+++ quantkits/datayes/api.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*- 
+"""
+通联数据
+Created on 2015/08/24
+@author: Jimmy Liu
+@group : waditu
+@contact: jimmysoa@sina.cn
+"""
+from quantkits.datayes.bond import Bond
+from quantkits.datayes.equity import Equity
+from quantkits.datayes.fund import Fund
+from quantkits.datayes.future import Future
+from quantkits.datayes.fundamental import Fundamental
+from quantkits.datayes.HKequity import HKequity
+from quantkits.datayes.idx import Idx
+from quantkits.datayes.macro import Macro
+from quantkits.datayes.master import Master
+from quantkits.datayes.options import Options
+from quantkits.datayes.market import Market
+from quantkits.datayes.subject import Subject
+from quantkits.datayes.IV import IV
+    
